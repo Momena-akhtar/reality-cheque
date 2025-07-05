0bfb902e825f746ba4188e958ed7bebb7a552aa7
Momena <152077758+Momena-akhtar@users.noreply.github.com>
2025-07-05 14:31:27 +0500

    add UI of getstarted (refined)

---
+++ app/components/ui/get-started.tsx
@@ -0,0 +1,10 @@
+import { ArrowRight } from "lucide-react";
+
+export default function GetStarted() {
+    return (
+        <button className="px-6 py-3 text-md cursor-pointer font-semibold bg-primary border-2 border-primary text-white rounded-xl hover:bg-primary-hover hover:border-primary-hover transition-all duration-300 flex items-center gap-3 shadow-lg shadow-primary/20 hover:shadow-primary/40 hover:scale-[1.02] hover:-translate-y-[1px]">
+            <ArrowRight className="w-5 h-5" />
+            Get Started
+        </button>
+    )
+}
