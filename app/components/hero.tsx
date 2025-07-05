d736f2767782d9d0219e5c701fcd7d5388199ef9
Momena <152077758+Momena-akhtar@users.noreply.github.com>
2025-07-05 14:30:57 +0500

    add the hero section (base)

---
+++ app/components/hero.tsx
@@ -0,0 +1,22 @@
+import GetStarted from "./ui/get-started";
+import ContactSales from "./ui/contact-sales";
+
+export default function Hero() {
+    return (
+        <div className="max-w-2xl mx-auto py-20 px-4">
+            <div className="space-y-5 text-center">
+                <h1 className="text-5xl font-bold leading-tight">
+                    Talk to the Right Bot,<br />
+                    Every Time.
+                </h1>
+                <p className="text-lg text-primary-text-faded max-w-lg mx-auto">
+                    Explore a plug-and-play interface for in-context, prompt-engineered AI agents
+                </p>
+                <div className="flex gap-4 pt-10 justify-center">
+                    <GetStarted />
+                    <ContactSales />
+                </div>
+            </div>
+        </div>
+    )
+}       
