6328001ce77b688ea2acfcd0a983cc57ff521786
Momena <152077758+Momena-akhtar@users.noreply.github.com>
2025-07-05 14:31:52 +0500

    add UI of contact sales (base)

---
+++ app/components/ui/contact-sales.tsx
@@ -0,0 +1,9 @@
+import { Phone } from "lucide-react";   
+export default function ContactSales() {
+    return (
+        <button className="px-4 py-2 text-md border cursor-pointer border-border text-white rounded-xl hover:bg-primary-hover transition-colors flex items-center gap-2">
+            <Phone className="w-4 h-4" />
+            Contact Sales
+        </button>
+    )
+}
