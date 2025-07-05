025f9f9ef4a038c00f59a6bcc79fb459910c1c1b
Momena <152077758+Momena-akhtar@users.noreply.github.com>
2025-07-05 23:04:59 +0500

    add dynamic bot card

---
+++ app/components/ui/bot-card.tsx
@@ -0,0 +1,47 @@
+import { ExternalLink } from "lucide-react";
+import CategoryTag from "./category-tag";
+import PaidButton from "./paid-button";
+import FreeButton from "./free-button";
+
+interface BotCardProps {
+  name: string;
+  description: string;
+  logo: string;
+  category: string;
+  version: "Free" | "Paid";
+}
+
+export default function BotCard({
+  name,
+  description,
+  logo,
+  category,
+  version,
+}: BotCardProps) {
+  return (
+    <div className="rounded-lg border border-border text-foreground shadow-sm bg-card transition-all duration-300 hover:scale-[1.02] hover:bg-card-hover">
+    <div className="flex flex-col space-y-1.5 p-6">
+        <div className="flex justify-between">
+            <img src={logo} alt={name} className="mb-2 h-10 w-10 rounded-md object-cover" />
+        </div>
+        <div role="heading" aria-level={3} className="font-semibold leading-none tracking-tight">
+            {name}
+        </div>
+        <p className="text-sm text-primary-text-faded line-clamp-2 h-10">
+            {description}
+        </p>
+    </div>
+    <div className="flex items-center border-t border-border p-4">
+        <div className="flex w-full items-center justify-between gap-2 text-muted-foreground">
+            <div className="flex items-center gap-2">
+                <CategoryTag category={category} />
+                {version === "Free"? <FreeButton /> : <PaidButton />}
+            </div>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+                <ExternalLink size={16} className="text-foreground hover:text-primary-text-hover transition-colors duration-150" />
+            </a>
+        </div>
+    </div>
+   </div>
+  );
+}
