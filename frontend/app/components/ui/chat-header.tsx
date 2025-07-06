b195ce8e17aaab086bef5c4ed7e3b58248482948
Momena <152077758+Momena-akhtar@users.noreply.github.com>
2025-07-06 11:01:34 +0500

    add logo and certain refinements

--- frontend/app/components/ui/chat-header.tsx
+++ frontend/app/components/ui/chat-header.tsx
@@ -1,15 +1,17 @@
+"use client"
 import Theme from "./theme";
 import UserIcon from "./user-icon";
 import SettingsIcon from "./settings";
+import Logo from "./logo";
 
 export default function ChatHeader() {
   return (
-    <div className="w-full backdrop-blur-sm bg-background border-border p-2 border-b">
+    <div className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center px-4 py-2 border-b border-border/40 bg-background/80 backdrop-blur-md supports-[backdrop-filter]:bg-background/60">
+      <Logo />
       <div className="flex justify-end gap-2">
-      <UserIcon />
-      <SettingsIcon />
+        <UserIcon />
+        <SettingsIcon />
         <Theme />
-       
       </div>
     </div>
   );
