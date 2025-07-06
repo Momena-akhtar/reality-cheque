36ceeefe828632bc7a4cd75871b0c060fbc461c5
Momena <152077758+Momena-akhtar@users.noreply.github.com>
2025-07-06 10:55:48 +0500

    fix chatbar colors and icons

--- frontend/app/components/ui/chat-bar.tsx
+++ frontend/app/components/ui/chat-bar.tsx
@@ -2,23 +2,23 @@ import { Mic, Plus, Send, Voicemail } from "lucide-react";
 
 export default function ChatBar() {
     return (
-        <div className="fixed bottom-0 left-0 right-0 bg-card border-card  mb-10 rounded-4xl border-border p-4 mx-auto max-w-4xl">
+        <div className="fixed bottom-0 left-0 right-0 bg-card border border-border mb-10 rounded-4xl border-border p-4 mx-auto max-w-4xl">
             <div className="flex items-center gap-2 mt-0 relative">
                 <input 
                     type="text"
                     placeholder="Ask me anything..."
-                    className="w-full p-3 pr-12 bg-card focus:outline-none focus:ring-2 focus:ring-primary/10 transition-all"
+                    className="w-full p-3 pr-12 bg-card focus:outline-none border-border  focus:ring-2 focus:ring-primary/10 transition-all"
                 />
                 <button 
-                    className="absolute right-3 p-2 text-foreground/80 hover:text-primary rounded-xl transition-colors"
+                    className="absolute right-0 p-2 text-foreground/80 hover:text-foreground rounded-xl transition-colors cursor-pointer "
                     aria-label="Send message"
                 >
-                    <Send className="w-5 h-5" />
+                    <Send width={18} height={18} />
                 </button>
             </div>
             <div className="flex justify-start gap-2 relative mt-2">
-                <Plus className="text-foreground/80 hover:text-foreground rounded-xl transition-colors" width={18} height={18}/>
-                <Mic className="text-foreground/80 hover:text-foreground rounded-xl transition-colors" width={18} height={18}/>
+                <Plus className="text-foreground/80 hover:text-foreground rounded-xl transition-colors cursor-pointer" width={18} height={18}/>
+                <Mic className="text-foreground/80 hover:text-foreground rounded-xl transition-colors cursor-pointer" width={18} height={18}/>
             </div>
         </div>
     )
