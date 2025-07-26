e8429ec4410b1e2ecae5fac9f9ddd05fdf9c11c7
Momena <momena.akhtar19@gmail.com>
2025-07-26 07:40:30 +0500

    create a simple admin signin page

---
+++ frontend/app/admin/page.tsx
@@ -0,0 +1,10 @@
+'use client';   
+import AdminSignInPopup from "../components/admin-signin";
+
+export default function AdminPage() {
+    return(
+        <div>
+            <AdminSignInPopup onClose={() => {}} />
+        </div>
+    )
+}
