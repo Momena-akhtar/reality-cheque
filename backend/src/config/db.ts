15af13f46dd0fe17f13907c25f09536a99a72c16
Momena <momena.akhtar19@gmail.com>
2025-08-02 10:25:39 +0500

    add additional scripts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/menubot';
-console.log('Connecting to MongoDB at:', MONGO_URI);
+const nodeEnv = process.env.NODE_ENV || "development";
+const envPath = `.env.${nodeEnv}`;
+dotenv.config({ path: envPath });
+
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/realitycheque';
 
 export const connectToDb = async () => {
   try {
@@ -10,7 +14,7 @@ export const connectToDb = async () => {
       connectTimeoutMS: 10000,
       family: 4,
     });
-    console.log('Database connection successful');
+    console.log('Database connected at: ', MONGO_URI);
   } catch (error) {
     console.error('Database connection error:', error);
     process.exit(1); 
